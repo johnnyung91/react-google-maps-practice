9f6befd461bb9fd84c8dab88f960cb75fe2ac7b0
Johnny Ung <johnnyung91@gmail.com>
2020-07-30 03:45:56 +0000

    able to display map

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 
-export default function App(props) {
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+const center = {
+  lat: 34.076594,
+  lng: -118.007391
+};
+
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyDtcHhMj3M4jXBJK5GC8UDIZ8C_UZaJY1M',
+    libraries
+  });
+
+  if (loadError) return 'Error Loading Map';
+  if (!isLoaded) return 'Loading Maps';
+
   return (
-    <h1>BROWN SPOTS!</h1>
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={11}
+        center={center}
+      >
+      </GoogleMap>
+    </div>
   );
 }
